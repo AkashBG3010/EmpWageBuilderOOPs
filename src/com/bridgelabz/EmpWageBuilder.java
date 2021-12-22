e92713494065ba3ab6e9c64fcfb7ca8ee0b73462
akashbg3010 <akashbpura@gmail.com>
2021-12-22 21:17:06 +0530

    Updated with daily wage calculation

--- src/com/bridgelabz/EmpWageBuilder.java
+++ src/com/bridgelabz/EmpWageBuilder.java
@@ -3,8 +3,31 @@ package com.bridgelabz;
 public class EmpWageBuilder {
 
 	public static void main(String[] args) {
-
-		System.out.println("Welcome to Employee Wage Computation Program");
-
+			EmpCalculation emp = new EmpCalculation();
+			emp.empCheck();
+			DailyWage wage = new DailyWage();
+			wage.dailyWageCalculation();
+		}
+	}
+	class EmpCalculation {
+		public static int empHrs;
+		public void empCheck() {
+			int empCheck = (int)Math.floor(Math.random() * 10) % 2;
+			if(empCheck == 1) {
+				System.out.println("Employee is present");
+				empHrs = 8;
+			}
+			else{
+				System.out.println("Employee is absent");
+			}
+		}
+	}
+	class DailyWage{
+		public static final int WAGE_PER_HOUR = 20;
+		
+		public void dailyWageCalculation() {
+		int totalDailyWage = EmpCalculation.empHrs * WAGE_PER_HOUR;
+		System.out.println("Daily employee wage is "+totalDailyWage);
+		}
 	}
 }
